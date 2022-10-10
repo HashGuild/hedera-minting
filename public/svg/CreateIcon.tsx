c50158a0a0656c043e932d2762cf5ae2b8b9d22f
“ihtisham1211” <im.ihtisham.arif@gmail.com>
2022-10-10 08:32:29 +0200

    feat: new components for creating collection.

---
+++ public/svg/CreateIcon.tsx
@@ -0,0 +1,29 @@
+const CreateIcon = function (props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      width="45"
+      height="45"
+      viewBox="0 0 45 45"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <rect
+        x="0.684082"
+        y="0.575439"
+        width="44.2105"
+        height="43.8356"
+        rx="21.9178"
+        fill="#98A2B3"
+      />
+      <path
+        d="M22.7896 14.822V30.1645M15.0527 22.4933H30.5264"
+        stroke="white"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+export default CreateIcon;
