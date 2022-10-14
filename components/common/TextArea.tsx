016d122dd6f4c531f3ded9464ef3ed7ce76d2cad
“ihtisham1211” <im.ihtisham.arif@gmail.com>
2022-10-14 10:31:27 +0200

    feat: TextArea component.

---
+++ components/common/TextArea.tsx
@@ -0,0 +1,54 @@
+import React, { InputHTMLAttributes, useState } from 'react';
+import classNames from '../../utils/classNames';
+import ErrorMessage from './ErrorMessage';
+import Tooltip from './Tooltip';
+
+interface TextAreaProps extends InputHTMLAttributes<HTMLTextAreaElement> {
+  labelText?: string;
+  labelStyle?: string;
+  containerStyles?: string;
+  toolTipContent?: React.ReactNode;
+  iconLeft?: React.ReactNode;
+  iconRight?: React.ReactNode;
+  inputContainerStyles?: string;
+  error: boolean;
+  errorMessage: string;
+}
+
+const TextArea = function ({
+  labelText = '',
+  labelStyle = '',
+  containerStyles = '',
+  toolTipContent = '',
+  inputContainerStyles = '',
+  iconLeft,
+  iconRight,
+  error,
+  errorMessage,
+  ...props
+}: TextAreaProps) {
+  const [focus, setFocus] = useState(false);
+  return (
+    <div className={classNames('flex flex-col', containerStyles)}>
+      <p className={classNames('text-sm', labelStyle)}>{labelText}</p>
+      <div className="flex items-center">
+        <div
+          className={classNames(
+            'flex  items-center justify-center shadow-sm border w-full rounded-md px-2',
+            focus ? inputContainerStyles : '',
+          )}
+        >
+          {iconLeft && <div className="mr-1 ">{iconLeft}</div>}
+          <textarea onFocus={() => setFocus(true)} {...props} />
+          {toolTipContent && (
+            <Tooltip iconStyle="stroke-gray-400">{toolTipContent}</Tooltip>
+          )}
+        </div>
+        {iconRight && <div className="mx-2 ">{iconRight}</div>}
+      </div>
+      {focus && error && <ErrorMessage errorText={errorMessage} />}
+    </div>
+  );
+};
+
+export default TextArea;
