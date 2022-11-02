a3c9c537e5a235d1d10db984d55012405a222311
Alex Seidmann <alex@leanpnt.de>
2022-11-02 16:59:34 +0100

    fix: helper overlap

--- components/common/Modal.tsx
+++ components/common/Modal.tsx
@@ -20,15 +20,15 @@ const Modal: FC<ModalProps> = function ({
   };
   if (showModal) {
     return (
-      <div className="inset-0 fixed flex-col items-center bg-gray-900/50 z-10 min-w-full px-[5%] md:px-[15%] pt-10 md:pt-40">
-        <div className='max-w-xl mx-auto'>
-        <div>
-          <CrossIcon
-            className="mb-2 h-10 w-10 cursor-pointer"
-            onClick={closeModal}
-          />
-        </div>
-        <div className="bg-white p-8 rounded-md">{children}</div>
+      <div className="inset-0 fixed flex-col items-center bg-gray-900/50 z-30 min-w-full px-[5%] md:px-[15%] pt-10 md:pt-40">
+        <div className="max-w-xl mx-auto">
+          <div>
+            <CrossIcon
+              className="mb-2 h-10 w-10 cursor-pointer"
+              onClick={closeModal}
+            />
+          </div>
+          <div className="bg-white p-8 rounded-md">{children}</div>
         </div>
       </div>
     );
