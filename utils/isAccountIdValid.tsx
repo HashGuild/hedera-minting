b6ff7f3c9f4e97623173c457ee43f7515d6f1370
Daniel <daniel@hashguild.xyz>
2022-12-19 18:01:26 +0100

    fix: deploy break

--- utils/isAccountIdValid.tsx
+++ utils/isAccountIdValid.tsx
@@ -3,7 +3,7 @@ import { ChangeEvent, Dispatch, SetStateAction } from 'react';
 import { NftFormErrors, StepOneErrors } from './Interfaces';
 import getAccountInfo from './getAccountInfo';
 
- export default async function isAccountIdValid (e: ChangeEvent<HTMLInputElement>, formDataErrors: NftFormErrors | StepOneErrors, setFormDataErrors: Dispatch<SetStateAction<NftFormErrors | StepOneErrors>>) {
+ export default async function isAccountIdValid (e: ChangeEvent<HTMLInputElement>, formDataErrors: NftFormErrors | StepOneErrors, setFormDataErrors: Dispatch<SetStateAction<any>>) {
     if (e.target.name === 'accountId' && e.target.value?.length > 5) {
      const result = await getAccountInfo(e.target.value)
  
